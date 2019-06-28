02193a9a78ec773b2caf0d07dc994ad90d375a0f
narci <narci.ltc@toceansoft.com>
2019-06-28 19:16:37 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ import '@/assets/scss/common.scss'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/icons'
-import SvgIcon from '_c/common/SvgIcon.vue' // svg组件
+// import SvgIcon from '_c/common/SvgIcon.vue' // svg组件
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
 // 全局注册
-Vue.component('svg-icon', SvgIcon)
+// Vue.component('svg-icon', SvgIcon)
 
 new Vue({
   router,
